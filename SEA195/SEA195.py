0401f0dda17be7f82e2484785e2419555ac645e2
woohoohooh <woohoohooh@gmail.com>
2023-09-17 18:46:22 +0800

    1

---
+++ SEA195/SEA195.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import sys
+import urllib.request
+from PIL import Image
+
+name = 'SEA195'
+url = 'https://korter.ae/al-raha-lofts-two-abu-dhabi'
+
+r = requests.get(url=url)
+
+r = r.text
+
+a1 = r.find('imageType":"photo","src":"')
+a2 = r[a1+19:]
+a3 = a2.find('"}}}],')
+a4 = a2[:a3+1]
+
+b1 = a4.split(',')
+print(b1)
+count = 1
+img = 1
+lst = []
+for i in b1:
+    q1 = i.find('https://')
+    q2 = i.rfind('"')
+    q3 = i[q1:q2]
+    if '2560x1920' in q3 and q3 not in lst:
+        lst.append(q3)
+
+for i in lst:
+    urllib.request.urlretrieve(i, f"{name}-{img}.jpg")
+    img += 1
+    count += 1
